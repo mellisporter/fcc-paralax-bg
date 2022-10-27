7b3325e4b3d17ef82003ae010ba5c6474a08cfd0
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-27 12:15:35 -0400

    animation fucntion and bg layers

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 5;
+
+const backgroundLayer1 = new Image(); // creates a new image (built in class) and assigns each layer a picture source
+backgroundLayer1.src = 'backgroundLayers/layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'backgroundLayers/layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'backgroundLayers/layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'backgroundLayers/layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'backgroundLayers/layer-5.png';
+
+function animate() {
+    ctx.drawImage(backgroundLayer4, 0, 0);
+    requestAnimationFrame(animate);
+}
+
+animate();
